322909b7b0f4f76ff6a8d9cac27dae11d091ba5c
snehakarunakaran <skaruna1@hawk.iit.edu>
2015-11-29 13:23:51 -0600

    added css to the page and added links for gallery and introspection

--- index.php
+++ index.php
@@ -4,23 +4,43 @@
 <meta charset="UTF-8">
 </head>
 <body>
+<div align="right">
+<ul>
+<li><a href='gallery.php'/>View Images!</a></li>
+<li><a href='index.php'/>DB Backup!</a></li>
+</ul>
 
+</div>
+<div align="center">
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="result.php" method="POST">
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
-Enter Name of user: <input type="text" name="firstname"><br />
-    Send this file: <input name="userfile" type="file" accept="image/png,image/jpeg"/><br />
-Enter Email of user: <input type="email" name="useremail"><br />
-Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phone">
-
-
-<input type="submit" value="Send File" />
+<table>
+<tr>
+<td bgcolor="#7FFFD4">Enter Name of user: </td>
+<td><input type="text" name="firstname"></td>
+</tr>
+ <tr> 
+<td bgcolor="#7FFFD4">Send this file:</td>
+<td><input name="userfile" type="file" accept="image/png,image/jpeg"/></td>
+</tr>   
+<tr>
+<td bgcolor="#7FFFD4">Enter Email of user: </td>
+<td><input type="email" name="useremail"></td>
+</tr>
+<tr>
+<td bgcolor="#7FFFD4">Enter Phone of user (1-XXX-XXX-XXXX): </td>
+<td><input type="phone" name="phone"></td>
+</tr>
+<tr>
+<td colspan="2"><input type="submit" value="Send File" />
+</tr>
 </form>
 
 
-
+</div>
 
 </body>
 </html>
